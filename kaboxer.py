d2639d728beeaf4c8d0d2f13efd90fcbda45de52
Roland Mas <roland.mas@gnurandal.com>
2019-10-17 17:11:33 +0200

    More OO

--- kaboxer.py
+++ kaboxer.py
@@ -5,65 +5,74 @@ import yaml
 import os
 import sys
 
-def read_config(args):
-    paths = [
-        '.',
-        '/etc/kaboxer',
-        '/usr/share/kaboxer',
-    ]
-    for p in paths:
-        config_file = os.path.join(p,args.app+'.yaml')
-        if os.path.isfile(config_file):
-            try:
-                args.config = yaml.load(open(config_file))
-                return
-            except:
-                print("Error loading config file "+config_file)
-                sys.exit(1)
-    print("Missing config file")
-    sys.exit(1)
+import docker
 
-def run(args):
-    read_config(args)
-    pass
+class Kaboxer:
+    def __init__(self):
+        self.parser = argparse.ArgumentParser()
 
-def stop(args):
-    pass
+        subparsers = self.parser.add_subparsers(title='subcommands', help='action to perform', dest='action', required=True)
 
-def build(args):
-    pass
+        parser_run = subparsers.add_parser('run', help='run containerized app')
+        parser_run.add_argument('app')
+        parser_run.add_argument('executable', nargs='*')
+        parser_run.set_defaults(func=self.run)
 
-def pull(args):
-    pass
+        parser_stop = subparsers.add_parser('stop', help='stop running containerized app')
+        parser_stop.add_argument('app')
+        parser_stop.set_defaults(func=self.stop)
 
-def purge(args):
-    pass
+        parser_build = subparsers.add_parser('build', help='build image')
+        parser_build.add_argument('app')
+        parser_build.set_defaults(func=self.build)
 
-parser = argparse.ArgumentParser()
+        parser_pull = subparsers.add_parser('pull', help='fetch image')
+        parser_pull.add_argument('app')
+        parser_pull.set_defaults(func=self.pull)
 
-subparsers = parser.add_subparsers(title='subcommands', help='action to perform', dest='action', required=True)
+        parser_purge = subparsers.add_parser('purge', help='purge image')
+        parser_purge.add_argument('app')
+        parser_purge.set_defaults(func=self.purge)
 
-parser_run = subparsers.add_parser('run', help='run containerized app')
-parser_run.add_argument('app')
-parser_run.add_argument('executable', nargs='*')
-parser_run.set_defaults(func=run)
+    def go(self):
+        self.args = self.parser.parse_args()
+        print(self.args)
+        self.args.func()
 
-parser_stop = subparsers.add_parser('stop', help='stop running containerized app')
-parser_stop.add_argument('app')
-parser_stop.set_defaults(func=stop)
+    def run(self):
+        self.read_config()
+        print (self.config)
+        pass
 
-parser_build = subparsers.add_parser('build', help='build image')
-parser_build.add_argument('app')
-parser_build.set_defaults(func=build)
+    def stop(self):
+        pass
 
-parser_pull = subparsers.add_parser('pull', help='fetch image')
-parser_pull.add_argument('app')
-parser_pull.set_defaults(func=pull)
+    def build(self):
+        pass
 
-parser_purge = subparsers.add_parser('purge', help='purge image')
-parser_purge.add_argument('app')
-parser_purge.set_defaults(func=purge)
+    def pull(self):
+        pass
 
-args = parser.parse_args()
-print(args)
-args.func(args)
+    def purge(self):
+        pass
+
+    def read_config(self):
+        paths = [
+            '.',
+            '/etc/kaboxer',
+            '/usr/share/kaboxer',
+        ]
+        for p in paths:
+            config_file = os.path.join(p,self.args.app+'.yaml')
+            if os.path.isfile(config_file):
+                try:
+                    self.config = yaml.load(open(config_file))
+                    return
+                except:
+                    print("Error loading config file "+config_file)
+                    sys.exit(1)
+        print("Missing config file")
+        sys.exit(1)
+
+kaboxer = Kaboxer()
+kaboxer.go()
