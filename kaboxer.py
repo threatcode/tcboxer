b14aaa86a295997c51aa0ff6044d50fe10136b1f
Roland Mas <roland.mas@gnurandal.com>
2019-10-18 19:47:46 +0200

    Implement run()

--- kaboxer.py
+++ kaboxer.py
@@ -4,6 +4,7 @@ import argparse
 import yaml
 import os
 import sys
+import re
 
 import docker
 
@@ -34,6 +35,8 @@ class Kaboxer:
         parser_purge.add_argument('app')
         parser_purge.set_defaults(func=self.purge)
 
+        self.docker_conn = docker.from_env()
+
     def go(self):
         self.args = self.parser.parse_args()
         print(self.args)
@@ -41,8 +44,63 @@ class Kaboxer:
 
     def run(self):
         self.read_config()
-        print (self.config)
-        pass
+        print(self.config)
+        opts = {}
+        opts['environment'] = {}
+        opts['auto_remove'] = True
+        try:
+            netname = self.config['networks'][0]
+            self.create_network(netname)
+            opts['network'] = netname
+            extranets = self.config['networks'][1:]
+        except KeyError:
+            pass
+        try:
+            ports = {}
+            for publish_port in self.config['publish_ports']:
+                ports[publish_port] = publish_port
+            opts['ports'] = ports
+        except KeyError:
+            pass
+        try:
+            mounts = list(map (lambda x: docker.types.Mount(x['target'],x['source']), self.config['mounts']))
+            opts['mounts'] = mounts
+        except KeyError:
+            pass
+        run_mode = self.config['run_mode']
+        try:
+            image = self.config['image']
+        except KeyError:
+            image = self.args.app
+
+        if run_mode == 'cli':
+            opts['tty'] = True
+            opts['stdin_open'] = True
+        elif run_mode == 'gui':
+            xsock = '/tmp/.X11-unix'
+            xauth = os.path.join(os.getenv('HOME'), '.docker.auth')
+            f = subprocess.Popen(['xauth', 'nlist', os.getenv('DISPLAY')], stdout=subprocess.PIPE).stdout
+            g = subprocess.Popen(['xauth', '-f', xauth, 'nmerge', '-'], stdin=subprocess.PIPE).stdin
+            for l in f:
+                ll = re.sub('^....', 'ffff', l)
+                g.write(ll)
+            g.close()
+            opts['environment']['DISPLAY'] = os.getenv('DISPLAY')
+            opts['environment']['XAUTHORITY'] = xauth
+            opts['mounts'].extend(docker.types.Mount(xauth,xauth))
+            opts['mounts'].extend(docker.types.Mount(xsock,xsock))
+        elif run_mode == 'server':
+            opts['tty'] = True
+            opts['name'] = self.args.app
+        else:
+            print ("Unknown run mode")
+            sys.exit(1)
+        print(opts)
+        container = self.docker_conn.containers.create(image, *self.args.executable, **opts)
+        for e in extranets:
+            create_network(e).connect(container)
+        container.start()
+        return
 
     def stop(self):
         pass
@@ -73,6 +131,12 @@ class Kaboxer:
                     sys.exit(1)
         print("Missing config file")
         sys.exit(1)
+
+    def create_network(self, netname):
+        for n in self.docker_conn.networks.list():
+            if n.name == netname:
+                return n
+        return self.docker_conn.networks.create(name=netname, driver='bridge')
 
 kaboxer = Kaboxer()
 kaboxer.go()
