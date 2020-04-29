d973ba02af2c8360773a4a7c08fa873c1731cf2b
Roland Mas <roland.mas@gnurandal.com>
2020-04-29 12:46:58 +0200

    Refactor a bit

---
+++ tests/fixtures/run.sh
@@ -0,0 +1,16 @@
+#! /bin/sh
+
+case "$1" in
+    hi)
+	echo "Hi there"
+	;;
+    sleep)
+	sleep 60
+	;;
+    demo)
+	cat /kbx-demo.txt /kaboxer/version
+	if [ -d /persist ] ; then
+	    v=$(cat /kaboxer/version)
+	    echo "Running version $v at $(date)" >> /persist/run_history
+	fi
+esac
